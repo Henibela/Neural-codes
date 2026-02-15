d31b8a9d136678247f78023f8ce90f38b7d52bff
belabit <bela23bitew@gmail.com>
2026-02-15 17:08:47 +0300

    math:defined x as a perceptron

--- sigmoid_plot.py
+++ sigmoid_plot.py
@@ -4,7 +4,11 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x)) 
 
-x = np.linspace(-10, 10, 1000)
+w = -2
+b = 3
+z = np.linspace(-10, 10, 1000)
+x = w*z + b
+
 y = sigmoid(x)
 plt.plot(x, y)
 plt.title('Sigmoid Function')
